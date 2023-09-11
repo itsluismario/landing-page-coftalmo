48cf6f74318aeee5b50adde17332c0a7b75f3661
itsluismario <itsluismario@gmail.com>
2023-09-11 16:57:41 -0600

    :bug: Getting rid of img

--- src/app/components/Team.tsx
+++ src/app/components/Team.tsx
@@ -59,11 +59,6 @@ export default function Team() {
             {teamMembers.map((member, index) => (
               <li key={index}>
                 <div className="flex items-center gap-x-6">
-                  {/* <img
-                    className="h-16 w-16 rounded-full"
-                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  /> */}
                   <div>
                     <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
                       {member.name}
