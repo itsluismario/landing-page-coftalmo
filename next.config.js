baab828a5a531d22a60dffbc023a859e9ed69cb6
itsluismario <itsluismario@gmail.com>
2023-09-11 18:28:03 -0600

    :adhesive_bandage: Fixing images in SeenUs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['imgs.search.brave.com'], // Add your domain(s) here
+      },
+}
 
 module.exports = nextConfig
--- src/app/components/SeenUs.tsx
+++ src/app/components/SeenUs.tsx
@@ -13,18 +13,20 @@ export default function SeenUs() {
             <li className="flex justify-center items-center"> {/* Center the content */}
               <div>
                 <Image
-                  className="h-40 w-40"
                   src="https://imgs.search.brave.com/RNlP-lFZo6wUkUFSgefCWc4BmO3IFClvN4gJM-hAj9M/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODQyZjY4ZmE2NTE1/YjFlMGFkNzViMjIu/cG5n"
-                  alt=""
+                  width={150}
+                  height={150}
+                  alt="Harvard logo"
                 />
               </div>
             </li>
             <li className="flex justify-center items-center"> {/* Center the content */}
               <div>
                 <Image
-                  className="h-30 w-80"
                   src="https://imgs.search.brave.com/JgIhTUqbkDKv9rzgV0S1yZ0cmM5In9vhxoL1YV1KEIM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9maW5k/dmVjdG9ybG9nby5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTkvMTEvdGVjbm9s/b2dpY28tZGUtbW9u/dGVycmV5LXZlY3Rv/ci1sb2dvLnBuZw"
-                  alt=""
+                  width={500}
+                  height={500}
+                  alt="Tec logo"
                 />
               </div>
             </li>
